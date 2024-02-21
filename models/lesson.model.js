89840e768f25f23badd5e0c963a28be39edc775c
4ebyrek <vasya-korn71@mail.ru>
2024-02-21 16:12:34 +0600

    Вернул время в модель расписания

--- models/lesson.model.js
+++ models/lesson.model.js
@@ -12,6 +12,14 @@ const lessonSchema = new Schema({
                 type: String,
                 required: true
             },
+            timeStart: {
+                type: String,
+                required: true
+            },
+            timeEnd: {
+                type: String,
+                required: true
+            },
             zoomId: {
                 type: String,
                 required: false
